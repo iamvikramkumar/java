709848da1c13b9e6168f5a7b37904d09d4d26f6f
iamvikramkumar <iamvikramkumar5@gmail.com>
2024-10-24 01:01:10 +0530

    enum

---
+++ collections/Deparment.java
@@ -0,0 +1,7 @@
+package collections;
+
+public enum Deparment {
+    LEARNING,DEVELOPMENT,TESTING
+}
+
+//An enum is a special "class" that represents a group of constants (unchangeable variables, like final variables).
---
+++ collections/Gender.java
@@ -0,0 +1,8 @@
+package collections;
+
+public enum Gender {
+    MALE, FEMALE
+}
+
+
+// An enum is a special "class" that represents a group of constants (unchangeable variables, like final variables).
