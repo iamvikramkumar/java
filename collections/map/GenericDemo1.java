1e7941bed27ebaff90bcfa0e7fed0e7b6f630978
iamvikramkumar <iamvikramkumar5@gmail.com>
2024-10-31 00:00:59 +0530

    map

---
+++ collections/map/GenericDemo1.java
@@ -0,0 +1,24 @@
+package collections.map;
+
+public class GenericDemo1 {
+    public static void main(String[] args) {
+        new GenericDemo1().f1();
+    }
+
+    private void f1() {
+    Set<String> l=new TreeSet<String>();
+    l.add("PRaveen");
+    l.add("Ajay");
+    l.add("Prudhvi");
+    l.add("Australia");
+    l.add("india");
+
+   Iterator<String> iterator= l.iterator();
+   while(iterator.hasNext()){
+       String s=iterator.next();
+       System.out.println(s);
+   }
+
+
+    }
+}
