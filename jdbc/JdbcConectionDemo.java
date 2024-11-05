e493c2a1a95033ef057be75fcaf8c0b030de3470
iamvikramkumar <iamvikramkumar5@gmail.com>
2024-11-05 22:50:17 +0530

    jdbc_connection_demo

---
+++ jdbc/JdbcConectionDemo.java
@@ -0,0 +1,31 @@
+package com.capg.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcConectionDemo {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		Class.forName("org.postgresql.Driver");
+
+Connection connection = 
+DriverManager.getConnection("jdbc:postgresql://localhost:5432/capg_db", "postgres",
+		"elephant");
+
+		Statement st = connection.createStatement();
+
+		int updateStatus = st.executeUpdate("insert into employ values(1001,'arun',45000.25)");
+
+		if (updateStatus > 0) {
+			System.out.println("row inserted successfully");
+		} else {
+			System.out.println("row not inserted");
+		}
+
+		connection.close();
+
+	}
+}
