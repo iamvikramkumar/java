3554f8ee3e0f712753f7067ac836e620523bf44a
iamvikramkumar <iamvikramkumar5@gmail.com>
2024-11-05 23:23:32 +0530

    jdbc_con_demo

--- jdbc/ConnDemo.java
+++ jdbc/ConnDemo.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class JdbcConectionDemo {
+public class ConnDemo {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
